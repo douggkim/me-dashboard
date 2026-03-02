9b3e35f21d5d3dbf47229584250d8ddbaaa14745
slakingex@gmail.com <slakingex@gmail.com>
2026-03-01 17:52:57 -0800

    feat: increase max concurrent runs and executor concurrency to 4

--- src/main.py
+++ src/main.py
@@ -68,5 +68,5 @@ defs = dg.Definitions(
     schedules=[weekly_delta_maintenance_schedule],
     sensors=[email_failure_sensor],
     asset_checks=dg.load_asset_checks_from_package_module(asset_checks_module),
-    executor=dg.multi_or_in_process_executor.configured({"max_concurrent": 2}),
+    executor=dg.multi_or_in_process_executor.configured({"max_concurrent": 4}),
 )
